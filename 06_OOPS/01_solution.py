7f62006e52f08d189392b2b5a9370cb5aee292a2
Abhinav Sharma <abhi5237@gmail.com>
2025-04-09 22:28:33 +0530

    update

---
+++ 06_OOPS/01_solution.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        
+myCar = Car("Mitsubishi", "Lancer")
+print(myCar.brand)
+print(myCar.model)
+
+myNewCar = Car("Mahindra", "BE69")
+print(myNewCar.brand)
+print(myNewCar.model)
+
+
+
